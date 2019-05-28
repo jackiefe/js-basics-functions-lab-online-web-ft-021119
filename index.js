bf8d0d5c182b8982aaa0b8b858e525027a0646e8
Jacqi Entzminger <jackie.entz@gmail.com>
2019-05-28 21:22:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ function distanceTravelledInFeet(startingPoint,endingPoint) {
 }
 
 function calculatesFarePrice(start, destination) {
-  
+  let distance = distanceTravelledInFeet(startingPoint,endingPoint);
+  let result;
+
+  if (distance <= 400) {
+    result = 0;
+  }
+  else if (distance > 400 && distance < 2000) {
+    result = (distance - 400) * .02;
+  }
+
+  else if (distance > 2000 && distance < 2500) {
+    result = 25.00;
+  }
+
+  else if (distance > 2500) {
+    result = 'cannot travel that far';
+  }
+  return result
+}
 }
